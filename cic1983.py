273af9c1dcf85034e6a92ea0dc4a6ace78920a5e
Nicholas Escalona <nicholas.escalona@gmail.com>
2017-03-31 22:35:38 -0400

    latin

--- cic1983.py
+++ cic1983.py
@@ -5,9 +5,13 @@ import re
 import aiohttp
 import discord
 
-def canon_to_str(c_num, p_num1=None, p_num2=None, n_num1=None, n_num2=None):
+def canon_to_str(c_num, p_num1=None, p_num2=None, n_num1=None, n_num2=None,
+                 latin=False):
     strs = ['**Can. ', c_num, '** — ']
-    canon = canons[c_num]
+    if latin:
+        canon = latincanons[c_num]
+    else:
+        canon = canons[c_num]
     if p_num1 is None:
         if n_num1 is None:
             if canon['text']:
@@ -102,15 +106,16 @@ def canon_to_str(c_num, p_num1=None, p_num2=None, n_num1=None, n_num2=None):
                             strs.append(num['text'])
     return ''.join(strs)
 
-def query_to_str(c_num1, c_num2, p_num1, p_num2, n_num1, n_num2):
+def query_to_str(c_num1, c_num2, p_num1, p_num2, n_num1, n_num2, latin=False):
     strs = []
     if c_num2 is None:
-        strs.append(canon_to_str(c_num1, p_num1, p_num2, n_num1, n_num2))
+        strs.append(canon_to_str(c_num1, p_num1, p_num2, n_num1, n_num2,
+                                 latin))
     else:
         for i in range(int(c_num1), int(c_num2) + 1):
             if i > int(c_num1):
                 strs.append('\n')
-            strs.append(canon_to_str(str(i)))
+            strs.append(canon_to_str(str(i), latin=latin))
     return ''.join(strs)
 
 logging.basicConfig()
@@ -118,6 +123,9 @@ logging.basicConfig()
 with open('cic1983.json') as f:
     canons = json.load(f)
 canons = {k: v for d in canons for k, v in d.items()}
+with open('cic1983latin.json') as f:
+    latincanons = json.load(f)
+latincanons = {k: v for d in latincanons for k, v in d.items()}
 
 if 'PORT' in os.environ:
     port = int(os.environ['PORT'])
@@ -127,6 +135,7 @@ else:
     connector = None
 
 client = discord.Client(connector=connector)
+member_self = {}
 
 @client.event
 async def on_message(message):
@@ -135,19 +144,26 @@ async def on_message(message):
         return
 
     match = re.search(
-        r'\bC(?:c|an(?:ons?|n)?)\.?\s*(\d+)(?:\s*[-–]\s*(\d+))?'
+        r'\bC(?:c|an(?:one?s?|n)?)\.?\s*(\d+)(?:\s*[-–]\s*(\d+))?'
         r'(?:\s*[§p][§ps]?\.?\s*(\d+)(?:\s*[-–]\s*(\d+))?)?'
         r'(?:\s*n?[ns]?\.?\s*(\d+)(?:\s*[-–]\s*(\d+))?\s*°?[°s]?)?',
         message.content, flags=re.I)
     if match is None:
         return
+    latin = 'latin' in str(message.channel)
     try:
-        msg = query_to_str(*match.groups())
+        msg = query_to_str(*match.groups(), latin=latin)
     except KeyError:
         return
     if len(msg) > 10000:
         return
+    nickname = ('1983 Codex Iuris Canonici' if latin else
+                '1983 Code of Canon Law')
+    if message.server is not None:
+        await client.change_nickname(member_self[message.server], nickname)
     await client.send_message(message.channel, msg)
+    if message.server is not None:
+        await client.change_nickname(member_self[message.server], '1983 CIC')
 
 
 @client.event
@@ -156,5 +172,8 @@ async def on_ready():
     print(client.user.name)
     print(client.user.id)
     print('------')
+    for server in client.servers:
+        member_self[server] = server.get_member(client.user.id)
+        await client.change_nickname(member_self[server], '1983 CIC')
 
 client.run('Mjg3MjU3MzMxMTQ4OTE0Njg5.C5spCQ.j5PmkJ9rGroN7qP6MyfD2213yKQ')
